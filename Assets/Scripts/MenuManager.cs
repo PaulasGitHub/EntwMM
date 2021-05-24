9c7690e806f4851ea5b4f05ad00b9fbc217c9a77
PaulasGitHub <64545568+PaulasGitHub@users.noreply.github.com>
2021-05-24 19:00:29 +0200

    when esc-button is hit: pause game, show pause menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -9,12 +9,16 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        menu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            menu.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
