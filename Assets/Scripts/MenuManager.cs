1ea633663db10ccb24a6b19e499b046ea7fcdb64
PaulasGitHub <64545568+PaulasGitHub@users.noreply.github.com>
2021-05-27 10:37:53 +0200

    add shader Graph

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,20 +6,28 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     public GameObject menu;
+    public static bool gameIsPaused;
 
     // Start is called before the first frame update
     void Start()
     {
         menu.SetActive(false);
+        gameIsPaused = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            menu.SetActive(true);
-            Time.timeScale = 0;
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
         }
     }
 
@@ -27,10 +35,21 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         menu.SetActive(false);
+        gameIsPaused = false;
+        Time.timeScale = 1;
     }
 
-    public void ReturnToGame()
+    public void Resume()
     {
+        gameIsPaused = false;
+        menu.SetActive(false);
+        Time.timeScale = 1;
+    }
 
+    void Pause()
+    {
+        gameIsPaused = true;
+        menu.SetActive(true);
+        Time.timeScale = 0;
     }
 }
